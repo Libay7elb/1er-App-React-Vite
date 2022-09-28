32c0e74f1664d8c3a3e7b2dd82d8a0726305e2f9
Patito <lucaheredia95@gmail.com>
2022-09-28 13:42:50 -0300

    Primer app con react

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,44 @@
+import {useState,useContext} from "react"
+import { TaskContext } from "../context/TaskContext"
+
+function TaskForm() {
+
+    const [title, setTittle] = useState("")
+    const [description, setDescription] = useState("")
+    const {crateTask} = useContext(TaskContext)
+
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(title,description)
+       crateTask({
+        title,
+        description
+       });
+       setTittle("");
+       setDescription("");
+    }
+
+  return (
+    <div  className="max-w-md mx-auto">
+        <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea ✏️:</h1>
+            <input placeholder="escribe tu tarea"
+                onChange={(e)=>setTittle(e.target.value)}
+                value={title}
+                className="bg-slate-300 p-3 w-full mb-2"
+                autoFocus
+            />
+            <textarea placeholder="Escribi la descrip de la tarea"
+                onChange={(e) => setDescription(e.target.value)}
+                className="bg-slate-300 p-3 w-full mb-2"
+                value={description}
+            ></textarea>
+            <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+        </form>  
+    </div>
+  )
+}
+
+export default TaskForm
---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+
+
+export const tasks = [
+  {
+    id: 0,
+    title: "1er tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "2da tarea",
+    description: "mi 2da tarea",
+  },
+  {
+    id: 2,
+    title: "3er tarea",
+    description: "mi 3er tarea",
+  },
+];
